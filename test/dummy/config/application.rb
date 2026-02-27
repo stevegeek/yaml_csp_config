c4437a49c195da3b70eaa0b884d4e42311eb973b
Stephen Ierodiaconou <stevegeek@gmail.com>
2026-02-27 17:53:28 +0100

    Clean up old code/comments

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -3,22 +3,11 @@ require_relative "boot"
 require "rails/all"
 
 Bundler.require(*Rails.groups)
-require "yaml_csp_config"
 
 module Dummy
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    if Gem::Version.new(Rails::VERSION::STRING) >= Gem::Version.new("5.1.0")
-      # Initialize configuration defaults for current Rails version.
-      config.load_defaults "#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}"
-    end
+    config.load_defaults "#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}"
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    # Required for Rails 5.0 and 5.1
     config.secret_key_base = SecureRandom.hex
   end
 end
